b3a6c181c0fdc5d4af16edf1309c430f81670047
Cjft92 <100280939+TheChandalorian@users.noreply.github.com>
2022-12-07 13:16:03 +0000

    Add files via upload
    the first of many code being added to my repository

---
+++ robotics_factory_IF_ELIF_ELSE.py
@@ -0,0 +1,9 @@
+amount = int(input('Enter the amount your wish to purchase:'))
+if(amount > 10000):
+    print('Head to tender to confirm order')
+elif(amount >500):
+    (amount <10000) 
+    print('You need to get quotes form 3 different suppliers')
+else:
+    print('Thats perfect, go ahead and order')
+        
