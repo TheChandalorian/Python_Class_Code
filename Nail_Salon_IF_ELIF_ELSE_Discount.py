bc66c417058d6297382129ca209b14540ade4116
Cjft92 <100280939+TheChandalorian@users.noreply.github.com>
2023-09-13 15:02:19 +0100

    Update Nail_Salon_IF_ELIF_ELSE_Discount.py

--- Nail_Salon_IF_ELIF_ELSE_Discount.py
+++ Nail_Salon_IF_ELIF_ELSE_Discount.py
@@ -4,14 +4,14 @@ nail_cost = buy_nails_amount / 10
  
 if buy_nails_amount >= 151:
     order_cost = nail_cost * 0.8
-    print('the cost of your order before P&P is: €' + str(round(order_cost,3)))
+    print('the cost of your order before P&P is: € ' + str(round(order_cost,3)))
 elif buy_nails_amount >= 51:
     order_cost = nail_cost * 0.9
-    print('the cost of your order before P&P is: €' + str(round(order_cost,3)))
+    print('the cost of your order before P&P is: € ' + str(round(order_cost,3)))
 else:
     order_cost = nail_cost
-    print('the cost of your order before P&P is: €' + str(round(order_cost,3)))
+    print('the cost of your order before P&P is: € ' + str(round(order_cost,3)))
 
 total_cost = float(order_cost) + 2.00
 
-print('The total cost of your order today inc P&P is: €' + str(round(total_cost,3)))
+print('The total cost of your order today inc P&P is: € ' + str(round(total_cost,3)))
