7a4067dcb11d9ae7eba5e7b3330ef3183f930721
Cjft92 <100280939+TheChandalorian@users.noreply.github.com>
2023-09-18 12:15:58 +0100

    Update Miles_Per_Galon.py

--- Miles_Per_Galon.py
+++ Miles_Per_Galon.py
@@ -1,4 +1,4 @@
 miles_driven = float(input('Enter mile: '))
 gallons_fuel = float(input('Enter Gallons: '))
-MPG = miles_driven / gallons_fuel
+MPG = miles_driven / gallons_fuel 
 print('The MPG is: ' + str(int(MPG)))
